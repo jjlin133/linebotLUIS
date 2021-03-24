f4f408897627d30b3cb6533126aeee149ef8356a
jjlin133 <63435164+jjlin133@users.noreply.github.com>
2021-03-24 18:26:43 +0800

    Update settings.py

--- linebotLUIS/settings.py
+++ linebotLUIS/settings.py
@@ -122,12 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = 'static'
+#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
 STATIC_URL = '/static/'
-if DEBUG:
-   STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'static'),
-   ]
-else:
-   STATIC_ROOT = os.path.join(BASE_DIR,'static')
-
-
+STATICFILES_DIRS = [  #加入 static 路徑
+	os.path.join(BASE_DIR, 'static'),
+]
