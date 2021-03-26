98ada0f3ba654fd9a287f5e2dafdf05848053ce5
jjlin133 <63435164+jjlin133@users.noreply.github.com>
2021-03-26 08:50:40 +0800

    Delete static/fv directory

--- static/fv/E_8_1_orig.js
+++
@@ -1,9 +0,0 @@
-function clubsavings() {
-    	var pv = parseInt(document.getElementById("amount").value);
-    	var i = parseFloat(document.getElementById("rate").value)/100;
-    	var n = parseInt(document.getElementById("period").value);
-    	var fv; var fvifa;
-    	fvifa = (( 1 + (i / 12)) * (Math.pow(( 1 + (i / 12)), n)-1)) / ( i / 12);
-    	fv = fvifa * pv;    	
-    	document.getElementById("result").innerHTML = "本利和 = $" + fv.toFixed(2);  
-}
